4d6db432e9cf5cb03f7686e09b754c7316b158c9
Chrxmee_Midnightt <8gxqw5b2h5@privaterelay.appleid.com>
2026-05-12 21:25:56 -0400

    Refactor GIF generation using gif-encoder-2

--- chrxmee-ai/commands/spin.js
+++ chrxmee-ai/commands/spin.js
@@ -1,7 +1,7 @@
 const { ChrxCommandBuilder } = require("chrxmaticc-framework");
 const { AttachmentBuilder } = require("discord.js");
 const { createCanvas, loadImage } = require("@napi-rs/canvas");
-const { GIFEncoder, quantize, applyPalette } = require("gifenc");
+const GIFEncoder = require("gif-encoder-2");
 
 module.exports = new ChrxCommandBuilder({
   name: "profile-spin",
@@ -17,11 +17,17 @@ module.exports = new ChrxCommandBuilder({
 
     try {
       const avatar = await loadImage(avatarURL);
-      const size = 512, frames = 24, delay = 4;
+      const size = 256, frames = 24, delay = 40;
+
+      const encoder = new GIFEncoder(size, size, "neuquant", true);
+      encoder.start();
+      encoder.setRepeat(0);
+      encoder.setDelay(delay);
+      encoder.setQuality(10);
+      encoder.setTransparent(0x00000000);
 
       const canvas = createCanvas(size, size);
       const ctx = canvas.getContext("2d");
-      const gif = GIFEncoder();
 
       for (let i = 0; i < frames; i++) {
         const angle = (i / frames) * Math.PI * 2;
@@ -31,14 +37,12 @@ module.exports = new ChrxCommandBuilder({
         ctx.rotate(angle);
         ctx.drawImage(avatar, -size / 2, -size / 2, size, size);
         ctx.restore();
-
-        const { data, width, height } = ctx.getImageData(0, 0, size, size);
-        const palette = quantize(data, 256);
-        gif.writeFrame(applyPalette(data, palette), width, height, { palette, delay });
+        encoder.addFrame(ctx);
       }
 
-      gif.finish();
-      const attachment = new AttachmentBuilder(Buffer.from(gif.bytes()), { name: `${target.username}-spin.gif` });
+      encoder.finish();
+      const gifBuffer = encoder.out.getData();
+      const attachment = new AttachmentBuilder(gifBuffer, { name: `${target.username}-spin.gif` });
       await interaction.editReply({ content: `🌀 **${target.displayName}** is SPINNING!`, files: [attachment] });
     } catch (err) {
       console.error("Spin error:", err);
