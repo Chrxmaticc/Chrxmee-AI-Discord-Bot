45e850350e927d84cdded031483862da15b8d07f
Chrxmee_Midnightt <8gxqw5b2h5@privaterelay.appleid.com>
2026-08-20 10:44:25 -0400

    Change avatar file format from JPEG to PNG

--- chrxmee-ai/index.js
+++ chrxmee-ai/index.js
@@ -16,9 +16,9 @@ function msToTime(ms) {
 
 // ==================== PFP ROTATION ====================
 const pfps = [
-  path.join(__dirname, "pfps", "pfp1.jpeg"),
-  path.join(__dirname, "pfps", "pfp2.jpeg"),
-  path.join(__dirname, "pfps", "pfp3.jpeg"),
+  path.join(__dirname, "pfps", "pfp1.png"),
+  path.join(__dirname, "pfps", "pfp2.png"),
+  path.join(__dirname, "pfps", "pfp3.png"),
 ];
 
 async function rotateAvatar(client) {
