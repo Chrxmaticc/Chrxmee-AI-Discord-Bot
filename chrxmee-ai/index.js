bdee362b6405bebb7de7b7f543e382a56e156f52
Chrxmee_Midnightt <8gxqw5b2h5@privaterelay.appleid.com>
2026-09-05 14:44:35 -0400

    Add GuildMessageReactions intent to bot client

--- chrxmee-ai/index.js
+++ chrxmee-ai/index.js
@@ -99,8 +99,9 @@ const client = new Client({
     GatewayIntentBits.DirectMessages,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessageReactions,
   ],
-  partials: [1, 3],
+  partials: [1, 2, 3],
 });
 
 client.commands = new Collection();
