07c009803ae0c268f5b5af49d3e5744451780ca8
Chrxmee_Midnightt <8gxqw5b2h5@privaterelay.appleid.com>
2026-09-17 15:28:43 -0400

    Add moderation module exports in index.js

---
+++ chrxmee-ai/cogs/moderation/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  store: require('./store'),
+  constants: require('./constants'),
+  cases: require('./cases'),
+  warns: require('./warns'),
+  escalation: require('./escalation'),
+  modlog: require('./modlog'),
+  raidMode: require('./raidMode'),
+  automod: require('./automod'),
+  engine: require('./engine'),
+};
