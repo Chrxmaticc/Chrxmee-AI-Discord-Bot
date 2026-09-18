b46e31b70facbd126582e32547b6420ac4c81349
Chrxmee_Midnightt <8gxqw5b2h5@privaterelay.appleid.com>
2026-09-17 21:35:13 -0400

    Update event name comment in automation_ready.js

--- chrxmee-ai/events/automation_ready.js
+++ chrxmee-ai/events/automation_ready.js
@@ -1,7 +1,7 @@
 const { scheduler } = require('../cogs/automation');
 
 module.exports = {
-  name: 'clientReady',   // ← swap to 'ready' if your discord.js is pre-v14.16
+  name: 'clientReady', // hi
   once: true,
   async execute(client) {
     scheduler.start(client);
